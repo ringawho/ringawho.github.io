26e325a58e9cacd040041331d5fb9bb574d7acee
ring <ringawho@sina.com>
2023-08-27 16:47:43 +0800

    first blog

---
+++ build-site.el
@@ -0,0 +1,58 @@
+(require 'ox-publish)
+(setq straight-vc-git-default-clone-depth 1)
+(setq straight-process-buffer " *straight-process*")
+(defvar bootstrap-version)
+(let ((bootstrap-file
+       (expand-file-name "straight/repos/straight.el/bootstrap.el" user-emacs-directory))
+      (bootstrap-version 6))
+  (unless (file-exists-p bootstrap-file)
+    (with-current-buffer
+        (url-retrieve-synchronously
+         "https://raw.githubusercontent.com/radian-software/straight.el/develop/install.el"
+         'silent 'inhibit-cookies)
+      (goto-char (point-max))
+      (eval-print-last-sexp)))
+  (load bootstrap-file nil 'nomessage))
+(straight-use-package 'htmlize)
+
+(setq base-directory "./content")
+(setq publishing-directory "./publish")
+(setq html-head (string-join
+                 (list "<link rel=\"stylesheet\" href=\"assets/theme.css\" type=\"text/css\" />"
+                       "<script type=\"text/javascript\" src=\"assets/theme.js\"></script>")
+                 "\n"))
+;; :html-head "<link rel=\"stylesheet\" href=\"https://cdn.simplecss.org/simple.min.css\" />"
+
+(setq org-publish-project-alist
+      `(("Blog Html"
+         :author "ring"
+         :base-directory ,base-directory
+         :base-extension "org"
+         :publishing-directory ,publishing-directory
+         :recursive t
+         :exclude "publish\\|README.org"
+         :publishing-function org-html-publish-to-html
+         :auto-sitemap t
+         :sitemap-filename "index.org"
+         :sitemap-title "ring"
+         ;; :makeindex t
+         :with-toc t
+
+         :html-validation-link nil
+         :html-checkbox-type html
+         :html-head-include-default-style nil
+         :html-head ,html-head)
+        ("Blog Stylesheet"
+         :base-directory ,base-directory
+         :base-extension "css\\|js\\|png"
+         :exclude "publish"
+         :publishing-directory ,publishing-directory
+         :recursive t
+         :publishing-function org-publish-attachment)
+        ("Blog"
+         :components ("Blog Html" "Blog Stylesheet"))))
+
+(org-publish-all t)
+
+(message "Build complete!")
+
